9ac0e5233784d7b19aeb4ece0bb8a54e37487384
PriyanshiSingh05 <168511987+PriyanshiSingh05@users.noreply.github.com>
2025-06-24 01:38:48 +0530

    Time: 16 ms (23.23%) | Memory: 45.4 MB (6.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        // creating a integer array to represent the frequency of each english letter  
-        int[] alphabet = new int[26];
-        for(int i = 0 ; i < s.length(); i++){
-            alphabet[s.charAt(i)-'a']++;
-
+        // hashmap frequency
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char p : s.toCharArray()){
+            map.put(p,map.getOrDefault(p,0)+1);
         }
-        for(int i =0; i < t.length();i++){
-            alphabet[t.charAt(i)- 'a']--;
-        }
-        for(int i = 0 ; i < alphabet.length; i++){
-            if(alphabet[i]!= 0){
+        for(char p : t.toCharArray()){
+            if(!map.containsKey(p)){
                 return false;
             }
+            //reduce count
+            map.put(p,map.get(p)-1);
+            if(map.get(p)<0) return false;//very imp check
         }return true;
 
     }
