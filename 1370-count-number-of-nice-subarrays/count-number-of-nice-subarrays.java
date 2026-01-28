3bfa43962067a241b53c6fa10513f2871076af1f
Priyanshi Singh <168511987+PriyanshiSingh05@users.noreply.github.com>
2026-01-29 02:38:01 +0530

    Time: 13 ms (69.43%) | Memory: 60 MB (6.08%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,23 +1,23 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         return atmostK(nums , k) - atmostK(nums , k -1 );
-     
-
 
     }
     public int atmostK(int[] nums , int k ){
         
         int left = 0;
         int count = 0 ;
+        int oddcount = 0;
 
         for(int right = 0 ; right < nums.length ; right++){
             if(nums[right] % 2 != 0){
-                k--;
+                //k--;
+                oddcount++;
             }
             // shrink window till k is valid 
-            while(k < 0){
+            while(k < oddcount){
                 if(nums[left] % 2 != 0){
-                    k++;
+                    oddcount--;
 
                 }
                 left++;         
